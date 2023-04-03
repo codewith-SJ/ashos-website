7a6ed2eaed465bab0ee68a26e336c872e9e3978d
ghostx31 <himanshu312001@gmail.com>
2023-04-03 18:07:17 +0530

    chore: setup project environment

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@catppuccin/palette'),
+    require('@catppuccin/tailwindcss')({
+      defaultFlavour: 'mocha',
+    }),
+  ],
 }
 
